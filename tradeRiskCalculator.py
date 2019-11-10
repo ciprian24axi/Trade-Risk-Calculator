c3a86bedce5ecb0917c78be3af21ef4d85ea0117
Ciprian <ciprian_axinte@icloud.com>
2019-11-10 12:36:45 +0000

    *

--- tradeRiskCalculator.py
+++ tradeRiskCalculator.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 
 window = Tk()
 window.title("Trade Risk Calculator")
-window.geometry("800x140")
+window.geometry("800x200")
 window.grid_columnconfigure((0,1), weight = 1)
 
 entryPrice = Label(window, text="Please insert the entry price: ", font=("Arial Bold", 16))
@@ -23,27 +23,37 @@ riskLevelcombo['values']= (1,2,3,4,5)
 riskLevelcombo.current(0)
 riskLevelcombo.grid(column = 1, row =2)
 
+answerUp = Label(window, text="", font=("Arial Bold", 16))
+answerUp.grid(column = 0, columnspan = 3, row = 6)
+
 answer = Label(window, text="", font=("Arial Bold", 16))
-answer.grid(column = 0, columnspan = 3, row = 6)
+answer.grid(column = 0, columnspan = 3, row = 7)
+
+answerDown = Label(window, text="", font=("Arial Bold", 16))
+answerDown.grid(column = 0, columnspan = 3, row = 8)
 
 def calculate():
+    riskLevelUp = float(riskLevelcombo.get()) + 1
+    riskLevelDown = float(riskLevelcombo.get()) - 1
     lossDistance = float(entryPricetxt.get()) - float(stopPricetxt.get())
     if lossDistance > 0:
         exitPrice = lossDistance * float(riskLevelcombo.get()) + float(entryPricetxt.get())
-        # exitPriceUp = lossDistance * riskLevelUp + entryPrice
-        # exitPriceDown= lossDistance * riskLevelDown + entryPrice
+        exitPriceUp = lossDistance * float(riskLevelUp) + float(entryPricetxt.get())
+        exitPriceDown= lossDistance * float(riskLevelDown) + float(entryPricetxt.get())
         # print('For a reduced risk, respectively a risk level of {}, please exit position at {}'.format(riskLevelDown, exitPriceDown))
         # print('For a increased risk, respectively a risk level of {}, please exit position {}'.format(riskLevelUp, exitPriceUp))
     elif lossDistance < 0:
         exitPrice = float(entryPricetxt.get()) + lossDistance * float(riskLevelcombo.get())
-        # exitPriceUp = entryPrice + lossDistance * riskLevelUp
-        # exitPriceDown = entryPrice + lossDistance * riskLevelDown
+        exitPriceUp = lossDistance * float(riskLevelUp) + float(entryPricetxt.get())
+        exitPriceDown= lossDistance * float(riskLevelDown) + float(entryPricetxt.get())
         # print('For a reduced risk, respectively a risk level of {}, please exit position at {}'.format(riskLevelDown, exitPriceDown))
         # print('For a increased risk, respectively a risk level of {}, please exit position {}'.format(riskLevelUp, exitPriceUp))
     elif lossDistance == 0:
         answer.configure(text = "The entry price can't be same with the entry price when you open the position")
     if lossDistance != 0:
-        answer.configure(text = 'For a risk level of {}, please exit position at {}'.format(riskLevelcombo.get(), str(exitPrice)[0:6]))
+        answerUp.configure(text = 'Feel risky today??? exit position at {}'.format(str(exitPriceUp)[0:6]), fg = "red")
+        answer.configure(text = 'For a risk level of {}, please exit position at {}'.format(riskLevelcombo.get(), str(exitPrice)[0:6]), fg="green")
+        answerDown.configure(text = 'Wanna play safe??? exit position at {}'.format(str(exitPriceDown)[0:6]), fg="blue")
         
 btn = Button(window, text="Calculate", command=calculate)
 btn.grid(column=0, columnspan = 3, row=5)
